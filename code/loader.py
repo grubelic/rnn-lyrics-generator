8f0423d395fcf29e4af6ac2454362c649c7aae38
Damjan <damjan.grubelic@gmail.com>
2022-01-06 02:56:55 +0100

    Suporting 150k and poetry dataset loading

--- code/loader.py
+++ code/loader.py
@@ -1,10 +1,60 @@
 import pandas as pd
 
-#TODO: add a proper doc-comment
+KAGGLE_150K_PATH = "../datasets/labeled_lyrics_cleaned.csv"
+KAGGLE_POETRY_PATH = "../datasets/PoetryFoundationData.csv"
 
-# Loads dataset from path into a pandas dataframe
-# returns pd.DataFrame with 3 columns: artist, seq and song
-def load_dataset(path="datasets/labeled_lyrics_cleaned.csv"):
-    return pd.read_csv(path).drop(columns=["label", "Unnamed: 0"])
-    
+
+def load_kaggle_150k(path: str = KAGGLE_150K_PATH) -> pd.DataFrame:
+    """
+    Load the Kaggle "150K Lyrics Labeled with Spotify Valence" dataset
+    https://www.kaggle.com/edenbd/150k-lyrics-labeled-with-spotify-valence
+
+    :param path: path to the dataset
+    :return: dataframe with 3 columns: artist, lyrics, title
+    """
+    df = pd.read_csv(
+        path,
+        usecols=["artist", "seq", "song"],
+        skipinitialspace=True
+    )
+    df.rename(columns={"seq": "lyrics", "song": "title"}, inplace=True)
+    df.drop_duplicates(inplace=True, ignore_index=True)
+    return df
+
+
+def replace_in_file(path: str, old: bytes, new: bytes) -> None:
+    """
+    Replace all occurrences of old bytes with new bytes in a file whose path
+    is provided. Currently, loads the whole file in RAM.
+
+    :param path: path to the file whose content is being replaced
+    :param old: bytes that need to be replaced
+    :param new: bytes that need to be added
+    :return:
+    """
+    with open(path, 'rb') as inf:
+        data = inf.read().replace(old, new)
+    with open(path, 'wb') as outf:
+        outf.write(data)
+
+
+def load_kaggle_poetry(path: str = KAGGLE_POETRY_PATH) -> pd.DataFrame:
+    """
+    Load the Kaggle "Poetry Foundation Poems" dataset
+    https://www.kaggle.com/tgdivy/poetry-foundation-poems
+
+    :param path: path to the dataset
+    :return: dataframe with 3 columns: artist, lyrics, title
+    """
+    df = pd.read_csv(
+        path,
+        usecols=["Title", "Poem", "Poet"],
+        skipinitialspace=True
+    )
+    df.rename(columns={"Poem": "lyrics", "Title": "title", "Poet": "artist"},
+              inplace=True)
+    df.title = df.title.str.strip()
+    df.drop_duplicates(inplace=True, ignore_index=True)
+    return df
+
 
--- code/main.py
+++ code/main.py
@@ -1,6 +1,39 @@
 import loader
+import pandas as pd
+
+
+def print_weird_stuff(df: pd.DataFrame) -> None:
+    # work in progress
+    for text in df.lyrics:
+        for word in text.strip().split():
+            w: str = word.replace('\'', '')
+            if w.isalpha()\
+                or w.endswith('.') or w.endswith(',') or w.endswith('?'):
+                continue
+            print(w)
+
+
+def print_artists(df: pd.DataFrame) -> None:
+    print("\n".join(sorted(set(df.artist))))
+
+
+def get_lyrics(df: pd.DataFrame, artist: str, title: str) -> str:
+    return next(iter(df[(df.artist == artist) & (df.title == title)].lyrics))
+
+
+def get_song_names_by(df: pd.DataFrame, artist: str) -> pd.Series:
+    return df[(df.artist == artist)].title
+
 
 if __name__ == "__main__":
-    data = loader.load_dataset()
-    print(data.head())
+    # loader.replace_in_file(loader.KAGGLE_POETRY_PATH, b"\r", b"")
+    # data = loader.load_kaggle_poetry()
+    # lyrics = get_lyrics(data, "Connie Wanek", "Hartley Field")
+    # print(lyrics)
+    # print([it if it.isalpha() else ord(it) for it in lyrics])
+    data = loader.load_kaggle_150k()
+    # print(get_song_names_by(data, "Eminem"))
+    # print(get_lyrics(data, "Eminem", "Infinite"))
+    print_weird_stuff(data)
+
 
