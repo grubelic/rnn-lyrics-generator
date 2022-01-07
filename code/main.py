8ad6a40989107fb1e7bc08e3b7000fa3e3007b43
Damjan <damjan.grubelic@gmail.com>
2022-01-07 04:39:32 +0100

    Cleaning dataset - removing words in []

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import loader
 import pandas as pd
 from keras.preprocessing.text import Tokenizer
+import re
 
 
 def print_weird_stuff(df: pd.DataFrame) -> None:
@@ -27,15 +28,25 @@ def get_song_names_by(df: pd.DataFrame, artist: str) -> pd.Series:
     return df[(df.artist == artist)].title
 
 
+def clean_lyrics(df: pd.DataFrame) -> None:
+    in_brackets = re.compile(r"\[[^\n\]]*]")
+    """
+    for lyrics in df.lyrics:
+        found = in_brackets.findall(lyrics)
+        if found: print(found)"""
+    df.lyrics = df.lyrics.apply(lambda x: in_brackets.sub('\n', x))
+
+
 if __name__ == "__main__":
     # loader.replace_in_file(loader.KAGGLE_POETRY_PATH, b"\r", b"")
     # loader.replace_in_file(loader.KAGGLE_150K_PATH, b"\r", b"")
-    data = loader.load_kaggle_poetry()
-    lyrics = get_lyrics(data, "Connie Wanek", "Hartley Field")
+    # data = loader.load_kaggle_poetry()
+    # lyrics = get_lyrics(data, "Connie Wanek", "Hartley Field")
     # print(lyrics)
     # print([it if it.isalpha() else ord(it) for it in lyrics])
-    # data = loader.load_kaggle_150k()
-    # lyrics = get_lyrics(data, "LL Cool J", "#1 Fan")
+    data = loader.load_kaggle_150k()
+    clean_lyrics(data)
+    lyrics = get_lyrics(data, "LL Cool J", "#1 Fan")
     t = Tokenizer()
     t.fit_on_texts([lyrics])
     print(lyrics)
